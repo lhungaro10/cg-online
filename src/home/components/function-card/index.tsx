1155b3f7e5bf11d8eb4236763e40a0bf86c3cfde
Lucas Moreira Hungaro <lhungaro10@gmail.com>
2024-10-24 15:02:14 -0300

    fix: app links

--- src/home/components/function-card/index.tsx
+++ src/home/components/function-card/index.tsx
@@ -1,15 +1,23 @@
 import { ButtonHTMLAttributes } from "react"
 import { Container, Title } from "./styles"
+import { Link } from "react-router-dom"
 
 interface FunctionCardProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     title: string
+    nav: string
 }
 export function FunctionCard(props : FunctionCardProps) {
     return (
-        <Container
-            onClick={props.onClick}
+        
+        <Link
+            to={props.nav}
         >
-            <Title>{props.title}</Title>
-        </Container>
+            <Container
+                onClick={props.onClick}
+                >
+                <Title>{props.title}</Title>
+            </Container>
+        
+        </Link>
     )
 }
--- src/home/index.tsx
+++ src/home/index.tsx
@@ -3,21 +3,16 @@ import { FunctionCard } from "./components/function-card";
 import { MainContent } from "./styles";
 
 export function Home() {
-
-    const navigator = (key:string)=>{
-        window.location.href = key
-    }
-
     return(
         <>
             <Header 
                 title="Picasso Online"
             />
             <MainContent>
-            <FunctionCard title="Draw Line" onClick={() => navigator('draw-line')} />
-            <FunctionCard title="RGB -> HSV" onClick={() => navigator('convert-hsv')} />
-            <FunctionCard title="Draw Circle" onClick={() => navigator('draw-circle')} />
-            <FunctionCard title="House" onClick={() => navigator('small-house')} />
+            <FunctionCard title="Draw Line" nav={'/draw-line'}/>
+            <FunctionCard title="RGB -> HSV" nav={'/convert-hsv'} />
+            <FunctionCard title="Draw Circle" nav={'/draw-circle'} />
+            <FunctionCard title="House" nav={'/small-house'} />
 
             </MainContent>
         </>
