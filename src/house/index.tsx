9e014d863baa346f28225a2fd66d43aa6dd52f8a
Lucas Moreira Hungaro <lhungaro10@gmail.com>
2024-10-24 06:16:21 -0300

    fix: adjust house file

--- src/house/index.tsx
+++ src/house/index.tsx
@@ -19,62 +19,6 @@ export function House() {
         x: 0,
         y: 0
     });
-    function DrawLine(start: Cordinates, end: Cordinates){ {
-        const x1 = start.x;
-        const y1 = start.y;
-        const x2 = end.x;
-        const y2 = end.y;
-
-        const deltaY = y2 - y1;
-        const deltaX = x2 - x1;
-        const m = deltaY/deltaX;
-
-        if(y1 < y2){
-            for(let y = y1; y <= y2; y++){
-                const x = x1 + (y - y1)/m;
-                paintPixel(x, y);
-            }
-        }
-        else{
-            for(let y = y1; y >= y2; y--){
-                const x = x1 + (y - y1)/m;
-                paintPixel(x, y);
-            }
-        }
-        if(x1 < x2){
-            for (let x = x1; x <= x2; x++) {
-                const y = m * (x - x1) + y1;
-                paintPixel(x, y);
-            }
-        }else{
-            for (let x = x1; x >= x2; x--) {
-                const y = m * (x - x1) + y1;
-                paintPixel(x, y);
-            }
-        }
-
-    }
-    }
-
-    const constructHouse = ():void =>{
-        const point00:Cordinates = {
-            x: 10,
-            y: 10
-        }
-        const point0100:Cordinates = {
-            x: 10,
-            y: 100
-        }
-
-        const point50150:Cordinates = {
-            x: 60,
-            y: 150
-        }
-        
-        DrawLine(point00, point0100);
-        DrawLine(point0100, point50150);
-    }
-
     useEffect(()=>{
         clearCanvas();
         // constructHouse();
@@ -99,23 +43,16 @@ export function House() {
     }
 
     function paintPixel(x: number,y:number){
-        const webgl = canvasRef.current?.getContext('webgl2');
-        
-        console.log(webgl);
-        
-        // if(ctx){
-        //     ctx.beginPath();
-        //     ctx.fillStyle = 'red';
-        //     ctx.fillRect(x,y, 1,1);
-        //     ctx.closePath();
-        // }
+        const ctx = canvasRef.current?.getContext('2d');        
+        if(ctx){
+            ctx.beginPath();
+            ctx.fillStyle = 'red';
+            ctx.fillRect(x,y, 1,1);
+            ctx.closePath();
+        }
         
     }
 
-    function showMatrix(){
-        const ctx = canvasRef.current?.getContext('2d');
-        console.log(ctx?.getTransform());
-    }
 
     return (
         <>
@@ -127,7 +64,7 @@ export function House() {
                     width={500}
                     height={500}
                     ref={canvasRef}
-                    onClick={(e) =>{
+                    onClick={() =>{
                         // setStartCordinates([e.clientX, e.clientY]);
                         paintPixel(cordinates.x, cordinates.y);
                     }}  
